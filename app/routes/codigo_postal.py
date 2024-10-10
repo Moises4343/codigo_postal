a7fa3d5ca4740b653a8d4f54d2ad4d1f5ff9fb9d
Moises4343 <193243@ids.upchiapas.edu.mx>
2024-10-09 22:11:53 -0600

    add: endpoint con filtrado de campos

--- app/routes/codigo_postal.py
+++ app/routes/codigo_postal.py
@@ -2,14 +2,30 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.db.models import CodigoPostal
 from app.db.database import get_db
+from pydantic import BaseModel
+from typing import List
 
 router = APIRouter()
 
-@router.get("/api/v1/cp")
-def get_info_by_postal_code(cp: str, db: Session = Depends(get_db)):
-    result = db.query(CodigoPostal).filter(CodigoPostal.d_codigo == cp).all()
+class CodigoPostalResponse(BaseModel):
+    estado: str  
+    ciudad: str  
+    colonias: List[str]  
 
-    if not result:
+    class Config:
+        from_attributes = True  
+
+
+@router.get("/api/v1/postalCode", response_model=CodigoPostalResponse)
+def get_info_by_postal_code(cp: str, db: Session = Depends(get_db)):
+    results = db.query(CodigoPostal.d_estado, CodigoPostal.D_mnpio, CodigoPostal.d_asenta).filter(CodigoPostal.d_codigo == cp).all()
+
+    if not results:
         raise HTTPException(status_code=404, detail="No se encontró información para el código postal proporcionado.")
 
-    return result
+    estado = results[0].d_estado
+    ciudad = results[0].D_mnpio
+    colonias = [r.d_asenta for r in results]
+
+    return {"estado": estado, "ciudad": ciudad, "colonias": colonias}
+
