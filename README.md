68feb5605a5dd9c94afefa220a8bbce07aa47eca
Moises4343 <193243@ids.upchiapas.edu.mx>
2024-11-02 00:06:29 -0600

    add: especificaciones JSON API al consultar un código postal

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 #Crear archivo .env
 
 DATABASE_URL=mysql+mysqlconnector://usuario:password@localhost/nombre_DB
+
+#Ejecutar el código
+
+Posicionados en "servicios-codigo-postal" abrir la terminal y ejecutar
+
+--> uvicorn app.main:app --reload
--- app/routes/codigo_postal.py
+++ app/routes/codigo_postal.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app.db.models import CodigoPostal
 from app.db.database import get_db
@@ -7,25 +7,56 @@ from typing import List
 
 router = APIRouter()
 
+
+class CodigoPostalAttributes(BaseModel):
+    estado: str
+    ciudad: str
+    colonias: List[str]
+
+class CodigoPostalData(BaseModel):
+    type: str
+    id: str
+    attributes: CodigoPostalAttributes
+
 class CodigoPostalResponse(BaseModel):
-    estado: str  
-    ciudad: str  
-    colonias: List[str]  
+    data: CodigoPostalData
 
-    class Config:
-        from_attributes = True  
+class JSONAPIErrorDetail(BaseModel):
+    status: str
+    title: str
+    detail: str
 
+class JSONAPIErrorResponse(BaseModel):
+    errors: List[JSONAPIErrorDetail]
 
 @router.get("/api/v1/postalCode", response_model=CodigoPostalResponse)
 def get_info_by_postal_code(cp: str, db: Session = Depends(get_db)):
-    results = db.query(CodigoPostal.d_estado, CodigoPostal.D_mnpio, CodigoPostal.d_asenta).filter(CodigoPostal.d_codigo == cp).all()
+    results = db.query(
+        CodigoPostal.d_estado,
+        CodigoPostal.D_mnpio,
+        CodigoPostal.d_asenta
+    ).filter(CodigoPostal.d_codigo == cp).all()
 
     if not results:
-        raise HTTPException(status_code=404, detail="No se encontró información para el código postal proporcionado.")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No se encontró información para el código postal proporcionado."
+        )
 
     estado = results[0].d_estado
     ciudad = results[0].D_mnpio
     colonias = [r.d_asenta for r in results]
 
-    return {"estado": estado, "ciudad": ciudad, "colonias": colonias}
+    response = CodigoPostalResponse(
+        data=CodigoPostalData(
+            type="postalCodeInfo",
+            id=cp,
+            attributes=CodigoPostalAttributes(
+                estado=estado,
+                ciudad=ciudad,
+                colonias=colonias
+            )
+        )
+    )
 
+    return response
